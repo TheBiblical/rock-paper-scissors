36d148d80ae1781949cefd520e4a3336e64ed8d5
TheBiblical <joshuateohchiangyee@gmail.com>
2020-01-24 00:36:43 -0600

    123

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,3 +1,28 @@
 from random import randint
 
-print('hello')
+game = ['rock','paper','scissors']
+com = game[randint(0,2)] #return random integer
+player = False 
+
+while player == False:
+    input(print('Rock, Paper. or Scissors? \n'))
+    if player == com:
+        print('Same same!')
+    elif player == 'rock':
+        if com == 'paper':
+            print('I win, sucker!')
+        elif com == 'scissors':
+            print('Lucky')
+    elif player == 'paper':
+        if com == 'scissors':
+            print('I win, sucker!')
+        elif com == 'rock':
+            print('Lucky')
+    elif player == 'scissors':
+        if com == 'paper':
+            print('I win, sucker!')
+        elif com == 'rock':
+            print('Lucky')
+    player == False #set player to False to continue loop
+    
+
